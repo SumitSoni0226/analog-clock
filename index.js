5e97edffb7de696cc46d11a14823fad72b4bf693
Sumit Soni <sumit.soni@satschel.com>
2023-08-29 11:14:19 +0530

    fix: ui update

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+const dials = document.getElementsByClassName("clock-dials")[0];
+const dialLines = document.getElementsByClassName("diallines");
+
+for (let i = 1; i < 61; i++) {
+    dials.innerHTML += "<div class='diallines'></div>";
+    dialLines[i - 1].style.transform = "rotate(" + 6 * i + "deg)";
+}
+
 const hour = document.querySelector(".hours");
 const minute = document.querySelector(".minutes");
 const second = document.querySelector(".seconds");
 
-setInterval(()=>{
+setInterval(() => {
     const d = new Date();
     const htime = d.getHours();
-    const mtime= d.getMinutes();
+    const mtime = d.getMinutes();
     const stime = d.getSeconds();
 
-    const hrotation = 30 * htime + mtime/2;
+    const hrotation = 30 * htime + mtime / 2;
     const mrotation = 6 * mtime;
     const srotation = 6 * stime;
 
 
-    hour.style.transform =  `rotate(${hrotation}deg)`;
-    minute.style.transform =  `rotate(${mrotation}deg)`;
-    second.style.transform =  `rotate(${srotation}deg)`;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
